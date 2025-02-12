6e4e31489fc924e3b4b2d0d6fc258bd2902de2dc
yVentus <davidhenriq97@gmail.com>
2025-02-12 16:59:30 -0300

    feature/post_and_get

--- README.md
+++ README.md
@@ -3,3 +3,4 @@ Execute this command line in your console: `npm i express`
 
 --How to run--
 Execute this command line in your console: `node server.js`
+To run after every change: `node --watch server.js`
--- server.js
+++ server.js
@@ -1,9 +1,26 @@
-import express from 'express'
+import express from 'express';
 
-const app = express()
+const app = express();
+app.use(express.json()); /*Motivo: por padrão, express não usa json*/
+
+const users = [];
+
+app.post('/users', (request, response) => {
+    users.push(request.body),
+    console.log(request.body),
+    response.send('POST Users OK')
+});
 
 app.get('/users', (req, res) => {
-    res.send('Return Users OK')
-})
+    res.json(users)
+    //res.send('GET Users OK')
+});
 
-app.listen(3000)
+app.listen(3000);
+
+/* Criar API de usuários
+
+    -Criar usuário
+    -Listar usuários
+    -Editar usuários
+    -Deletar usuários*/
